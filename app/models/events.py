48e4122e612774c036bb1607329ad74149186fa3
Oleg Remizov <job.rem1zoff@gmail.com>
2024-01-02 14:10:54 +0300

    updated events routers [create and get]

--- app/models/events.py
+++ app/models/events.py
@@ -1,4 +1,4 @@
-from sqlmodel import JSON, SQLModel, Field, Column
+from sqlmodel import JSON, SQLModel, Field, Column, Text
 from typing import Optional, List
 
 
@@ -6,22 +6,21 @@ class Event(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
     title: str
     image: str
-    descripton: str
+    descripton: str =  Field(nullable=True)
     tags: List[str] = Field(sa_column=Column(JSON))
     location: str
-
 
     model_config = {
         # arbitrary_types - это параметр в Python, используемый при сериализации объектов с помощью модуля pickle
         # позволяет сериализать произвольные типы объектов, включая пользоватские классы, функции и другие объекты
         "arbitrary_types_allowed": True,
+        "orm_mode": True,
         "json_schema_extra": {
             "examples": [
-                {
-                    "id": 0,
+                { 
                     "title": "FastAPI Book Launch",
                     "image": "https://linktomyimage.com/image.png",
-                    "description": "BlaBlaBla...",
+                    "description": "Some text",
                     "tags": ["python", "fastapi"],
                     "location": "Google Meet"
                 }
--- app/routes/events.py
+++ app/routes/events.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Request, HTTPException, Depends, Body, status
-from typing import List
+from sqlalchemy import select, delete, insert, func, and_, or_
+from typing import List, Tuple
 
 from app.models.events import Event, EventUpdate
 from app.database.conections import get_session # маршруты смогут получить доступ к созданному объекту сеанса
@@ -14,27 +15,30 @@ events = []
 
 
 # Получение всех событий
-@router.get("/", response_model=List[Event])
-async def retrive_all_events() -> List[Event]:
+@router.get("/", response_model=List[Event]) 
+async def retrieve_all_events(session=Depends(get_session)) -> List[Event]:
+    statement = select(Event)
+    events = session.exec(statement).scalars().all() # метод возвращает список значений, а не объекты моделей
     return events
 
 
 # Получение события по id
 @router.get("/{id}", response_model=Event)
-async def retrive_event(id: int) -> Event:
-    for event in events:
-        if event.id == id:
-            return event
-    
-    raise HTTPException(
-        status_code=status. HTTP_404_NOT_FOUND,
-        detail="Event with supplied ID does not exist"
-    )
+async def retrive_event(id: int, session=Depends(get_session)) -> Event:
+    event = session.get(Event, id)
+    if not event: 
+        raise HTTPException(
+            status_code=status. HTTP_404_NOT_FOUND,
+            detail="Event with supplied ID does not exist"
+        )
+    return event
 
 # Создание нового события
 @router.post("/new")
-async def create_event(body: Event = Body(...)) -> dict:
-    events.append(body)
+async def create_event(new_event: Event, session=Depends(get_session)) -> dict:
+    session.add(new_event)
+    session.commit()
+    session.refresh(new_event)
     return {"message": "Event created successfully"}
 
 

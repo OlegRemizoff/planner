dadd2edeffb96ba65380a521b88efcde8674e24c
Oleg Remizov <job.rem1zoff@gmail.com>
2024-01-21 16:22:37 +0300

    pages_router & index.html

--- app/main.py
+++ app/main.py
@@ -1,13 +1,15 @@
 from fastapi import FastAPI
 import uvicorn
 
-from .routes.users import router as user_router
-from .routes.events import router as event_router
+from .routers.users import router as user_router
+from .routers.events import router as event_router
+from .routers.pages import router as pages_router
 
 
 app = FastAPI()
 app.include_router(user_router)
 app.include_router(event_router)
+app.include_router(pages_router)
 
 
 if __name__ == "__main__":

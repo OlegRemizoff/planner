3986fc3402cfd05758029a960c428396307f0b54
Oleg Remizov <job.rem1zoff@gmail.com>
2024-01-22 15:44:01 +0300

    temp

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 import uvicorn
+
 
 from .routers.users import router as user_router
 from .routers.events import router as event_router
@@ -7,6 +9,14 @@ from .routers.pages import router as pages_router
 
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="app/static"), "static")
+
+@app.get("/")
+async def read_root():
+    return {"message": "Hello, World"}
+
+
+
 app.include_router(user_router)
 app.include_router(event_router)
 app.include_router(pages_router)

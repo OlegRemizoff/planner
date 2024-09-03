08ce52974579505de89c200a1f3dbc09e83f380f
oleg_remizov <job.rem1zoff@gmail.com>
2024-09-03 14:49:32 +0300

    update check_text

--- app/routers/events.py
+++ app/routers/events.py
@@ -1,15 +1,17 @@
-from fastapi import APIRouter, Depends, HTTPException, status
-
+from fastapi import APIRouter, Depends, HTTPException, status, Response
+import requests
 from app.models.events import SEvent
 from app.models.users import Users
 from app.dao.planner_dao import EventDAO
 from app.dependencies import get_current_user
+from app.utils import check_text
 
 
 router = APIRouter(
     prefix="/event",
     tags=["Events"]
 )
+
 
 
 # Получение всех событий
@@ -34,7 +36,12 @@ async def retrive_event(id: int, user: Users = Depends(get_current_user)) -> SEv
 # Создание нового события
 @router.post("/new")
 async def create_event(data: SEvent) -> dict:
-    return await EventDAO.add_event(data)
+    raw_text = str(data.description) # забираем текст из JSON-формы и передаем его в check_text
+    correxted_text = await check_text(text=raw_text) 
+    print("\x1b[31;1m" + "До     " + "\x1b[0m", data.description)
+    print("\x1b[31;1m" + "После  " + "\x1b[0m", correxted_text)
+    return Response("Success!")
+    # return await EventDAO.add_event(data)
 
 
 # Обновление события
---
+++ app/utils.py
@@ -0,0 +1,21 @@
+import requests
+
+
+# Проверка орфографии с помощью Яндекс Спеллер
+YANDEX_SPELLER_API_URL = "https://speller.yandex.net/services/spellservice.json/checkText"
+
+
+async def check_text(text: str):
+    payload = {'text': text}
+    response = requests.get(YANDEX_SPELLER_API_URL, params=payload)
+    if response.status_code == 200:
+        data = response.json()
+        if data:
+            corrected_text = text
+            for error in data:
+                if error['s']:
+                    corrected_text = corrected_text.replace(
+                        error['word'], error['s'][0])
+            return corrected_text
+        
+    return text

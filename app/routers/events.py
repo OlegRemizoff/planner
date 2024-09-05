de49a5aeddbd230c286f0bba8b9a6f51b9f19e5a
oleg_remizov <job.rem1zoff@gmail.com>
2024-09-05 16:02:46 +0300

    user.id in create_event

--- app/routers/events.py
+++ app/routers/events.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter, Depends, HTTPException, status, Response
 import requests
+
 from app.models.events import SEvent
 from app.models.users import Users
-from app.dao.planner_dao import EventDAO
+from app.dao.planner_dao import EventDAO, UsersDAO
 from app.dependencies import get_current_user
 from app.utils import check_text
-from app.dependencies import get_current_user
 
 
 router = APIRouter(
@@ -34,11 +34,15 @@ async def retrive_event(id: int, user: Users = Depends(get_current_user)) -> SEv
 
 # Создание нового события
 @router.post("/new")
-async def create_event(data: SEvent) -> dict:
+async def create_event(data: SEvent, user: Users = Depends(get_current_user)) -> dict:
+    user_id = user.id
     raw_text = data.description # забираем текст из JSON-формы и передаем его в check_text
     correxted_text = await check_text(text=raw_text)
+    
     # print("\x1b[31;1m" + "До     " + "\x1b[0m", data.description)
     # print("\x1b[31;1m" + "После  " + "\x1b[0m", correxted_text)
+
+    data.user_id = user_id                  # меняем данные формы на id текущего пользователя
     data.description = str(correxted_text)  # меняем данные формы на корректные
     return await EventDAO.add_event(data)
 
